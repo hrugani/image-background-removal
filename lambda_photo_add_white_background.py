1e5ed7112ccb9e1045be679515238edd0877546c
Helio Rugani Brandao <hrugani@gmail.com>
2020-05-25 14:21:04 -0300

    removed unnecessary comments at the end of lambda_photp_add-white-background.py source file

--- lambda_photo_add_white_background.py
+++ lambda_photo_add_white_background.py
@@ -78,22 +78,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-# def download_from_bucket_no_background():
-#     pass
-
-# def upload_to_bucket_white_background(white_background_file_name):
-#     pass
-
-# file_path = sys.argv[1]
-# #load image with alpha channel.  use IMREAD_UNCHANGED to ensure loading of alpha channel
-# image = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
-# #make mask of where the transparent bits are
-# trans_mask = image[:,:,3] == 0
-# #replace areas of transparency with white and not transparent
-# image[trans_mask] = [255, 255, 255, 255]
-# #new image without alpha channel...
-# new_img = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
-# cv2.imwrite(os.path.basename(file_path) + "-while.png", new_img)
